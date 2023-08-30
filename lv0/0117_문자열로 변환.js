19099decc4c5b3ad7d8bfa77aaa3ba1c02fddbee
cxzaqq <roy1228@naver.com>
2023-08-30 21:33:41 +0900

    23.08.30

--- lv0/0117_문자열로 변환.js
+++ lv0/0117_문자열로 변환.js
@@ -3,3 +3,4 @@ const solution = (n) => n.toString();
 
 //most liked answer
 const solution1 = String;
+//타입 체크만 해서
---
+++ lv0/0120_문자열을 정수로 반환하기.js
@@ -0,0 +1,6 @@
+//my answer
+const solution = (n_str) => Number(n_str);
+
+//most liked answer
+const solution1 = Number;
+//타입 체크만 해서
