820e2c0afef72b0208b4836628fafb881eeb3e66
cxzaqq <roy1228@naver.com>
2023-09-01 01:15:45 +0900

    23.09.01

---
+++ lv0/0122_정수 부분.js
@@ -0,0 +1,13 @@
+//my answer
+const solution = (flo) => Math.floor(flo);
+
+/*
+Math.trunc() : 소수점 이하는 버린다
+Math.floor() : 내림(아래로)
+
+Math.trunc(-23.3) => -23
+Math.floor(-23.3) => -24
+
+문제에서 flo를 0이상으로 제한했기에 floor() 가능
+제한사항 잘 확인.
+*/
