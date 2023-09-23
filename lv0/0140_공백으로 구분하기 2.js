d23ed2a9d2679560d7f14b6c7f52716a0623827d
cxzaqq <roy1228@naver.com>
2023-09-23 20:52:02 +0900

    23.09.23

---
+++ lv0/0140_공백으로 구분하기 2.js
@@ -0,0 +1,8 @@
+//my answer
+const solution = (my_string) =>
+  my_string.split(" ").filter((e) => e.length > 0);
+
+//most liked answer
+function solution(my_string) {
+  return my_string.split(" ").filter((v) => v);
+}
