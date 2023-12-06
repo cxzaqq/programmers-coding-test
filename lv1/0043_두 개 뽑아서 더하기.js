eef96d28fc3f9fd677d3d24878a0ac9b94d02716
cxzaqq <roy1228@naver.com>
2023-12-06 17:58:52 +0900

    23.12.06

---
+++ lv1/0043_두 개 뽑아서 더하기.js
@@ -0,0 +1,27 @@
+//my answer
+const solution = (numbers) => {
+  let answer = [];
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      answer.push(numbers[i] + numbers[j]);
+    }
+  }
+  const set = new Set(answer);
+  answer = [...set];
+  return answer.sort((a, b) => a - b);
+};
+
+//most liked answer
+function solution(numbers) {
+  const temp = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      temp.push(numbers[i] + numbers[j]);
+    }
+  }
+
+  const answer = [...new Set(temp)];
+
+  return answer.sort((a, b) => a - b);
+}
