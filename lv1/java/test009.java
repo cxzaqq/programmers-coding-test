ee30b98bbeaf2741a57732c14554541b7d1cfa05
cxzaqq <roy1228@naver.com>
2025-07-03 16:43:23 +0900

    update

---
+++ lv1/java/test009.java
@@ -0,0 +1,19 @@
+package lv1.java;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class test009 {
+    public static void main(String[] args) {
+        String result = test009.solution("Zbcdefg");
+
+        System.out.println("result: " + result);
+        System.out.println(result.equals("gfedcbZ") ? "pass" : "fail");
+    }
+
+    public static String solution(String s) {
+        return s.chars().boxed().sorted(Comparator.reverseOrder())
+                .map(c -> String.valueOf((char) (int) c))
+                .collect(Collectors.joining());
+    }
+}
